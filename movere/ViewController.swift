e42affde00a3fa7f4fb07e28c2860f96a4e10e3a
Emily An <emilysuperhalo@gmail.com>
2022-08-02 15:27:30 -0400

    Initial Commit

---
+++ movere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movere
+//
+//  Created by Scholar on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
