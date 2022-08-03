5960208449a3e3750d99367fd57932290b498a7e
Rori <Rori.chanL1.9@gmail.com>
2022-08-03 09:31:53 -0400

    //Worked on the first view controller ("Hello,(user)"). //In addition, created the general layout for the rest of the view controllers except for the popup window for adding time/distance. Coding will be for in-person camp. //Check the viewController file for an attempt at making a chart feature--what's up with Swift Charts? It's still a Beta feature, yet, it's nowhere to be seen. //It was 1 AM. Music rules.

--- movere/ViewController.swift
+++ movere/ViewController.swift
@@ -4,17 +4,50 @@
 //
 //  Created by Scholar on 8/1/22.
 //
-
-import UIKit
+import SwiftUI
+//import Charts
 
 class ViewController: UIViewController {
-    
+//    struct barShape: Identifiable {
+//
+//        var type: String
+//        var count: Double
+//        var id = UUID()
+//    }
+//
+//    var data: [barShape] = [
+//        .init(type: "date", count: 4),
+//        .init(type: "date1", count: 5),
+//        .init(type: "date2", count: 1)
+//    ]
+//
+//    struct barChart: View {
+//        var body: some View {
+//           Chart {
+//           BarMark(
+//                x: .value("Shape Type", data[0].type),
+//                y: .value("Total Count", data[0].count)
+//                )
+//            BarMark(
+//                x: .value("Shape Type", data[1].type),
+//                y: .value("Total Count", data[1].count)
+//            )
+//            BarMark(
+//                x: .value("Shape Type", data[2].type),
+//                y: .value("Total Count", data[2].count)
+//                )
+//            }
+//
+//        }
+//    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
     }
-
-
 }
+    
+
+
+
 
